56c7558b43c20b6841e18edebb08b4a7ebdb54d2
David Shams <david.shams@gmail.com>
2018-06-26 10:56:28 -0500

    require('debug') not working console.log still shows

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
 
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello from express');
+    // res.send('Hello from express');
+    res.send('./index.html');
 })
 
 app.listen(3000, function(){
-    console.log('listening on port 3000...');
+    console.log(`listening on port ${chalk.green(3000)}` );
 });
