c39a958b9a09a1f2f6951a92a65615bc6c5130ad
David Shams <david.shams@gmail.com>
2018-06-26 00:10:11 -0500

    WORKING: full express app in 2 lines

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello from express');
+})
+
+app.listen(3000, function(){
+    console.log('listening on port 3000...');
+});
